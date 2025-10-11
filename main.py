2bd5d65ee0439473e4da487505df4590c046dd5e
Steven <vduong.steven@gmail.com>
2025-10-11 15:53:09 -0700

    First commit, with information on how to get started

---
+++ main.py
@@ -0,0 +1,11 @@
+# Authors: Steven Duong, Harry Lee, Anthony Trieu, Tony Wu
+# Project: CMPT 310 Final Project - Career Path Prediction
+# Date: Oct 11, 2025
+# Description: This file contains the code for the User Interface (UI) of the Career Path Prediction application.
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Set the title of the app
+st.title("Career Path Prediction")
